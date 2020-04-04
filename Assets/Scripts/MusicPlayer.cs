1b7ef3a53432e203bc25bd692b50143eff48a3d0
Sont3k <markus.residence@gmail.com>
2020-04-04 17:41:16 +0300

    97. Project Tidying Checklist

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,31 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    int firstLevelIndex = 1;
-
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Invoke("LoadFirstScene", 2f);
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(firstLevelIndex);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
